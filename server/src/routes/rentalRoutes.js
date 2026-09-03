f5965e96e48a000bb06063ad2050f61527c8f5a3
thasindu-dev <thasindu15tn@gmail.com>
2026-09-04 00:04:38 +0530

    Update rentalRoutes.js
    update and connect damage records,overdue rentals, cancel rentals etc

--- server/src/routes/rentalRoutes.js
+++ server/src/routes/rentalRoutes.js
@@ -8,6 +8,11 @@ const {
   rejectRental,
   issueRental,
   returnRental,
+  getAllDamageRecords,
+  updateDamageRecordStatus,
+  getOverdueRentals,
+  getMyRentals,
+  cancelRental,
 } = require("../controllers/rentalController");
 
 const authenticate = require("../middleware/authenticate");
@@ -36,6 +41,29 @@ router.get(
   authenticate,
   authorizeRoles("STAFF_ADMIN"),
   getAllRentals
+);
+
+//get damage records
+router.get(
+  "/damage-records",
+  authenticate,
+  authorizeRoles("STAFF_ADMIN"),
+  getAllDamageRecords
+);
+
+//Detect overdue rentals
+router.get(
+  "/overdue",
+  authenticate,
+  authorizeRoles("STAFF_ADMIN"),
+  getOverdueRentals
+);
+
+//customer rental history
+router.get(
+  "/my-rentals",
+  authenticate,
+  getMyRentals
 );
 
 //approve rental
@@ -69,4 +97,22 @@ router.patch(
   authorizeRoles("STAFF_ADMIN"),
   returnRental
 );
+
+//damage status update endpoint
+router.patch(
+  "/damage-records/:id/status",
+  authenticate,
+  authorizeRoles("STAFF_ADMIN"),
+  updateDamageRecordStatus
+);
+
+//cancelRental
+router.patch(
+  "/:id/cancel",
+  authenticate,
+  cancelRental
+);
+
+
+
 module.exports = router;
