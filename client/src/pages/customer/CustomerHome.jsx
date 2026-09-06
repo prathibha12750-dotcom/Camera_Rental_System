05412d3c9782892b44e67bb166c0e2fd70d63ddb
thasindu-dev <thasindu15tn@gmail.com>
2026-09-06 11:12:37 +0530

    Update CustomerHome.jsx
    linked equipment route to the CustomerHome.jsx and made equipment and rental card clikable

--- client/src/pages/customer/CustomerHome.jsx
+++ client/src/pages/customer/CustomerHome.jsx
@@ -1,7 +1,14 @@
 import { useAuth } from "../../context/useAuth";
 
+// linked equipment route by thasindu ================
+import { Link } from "react-router-dom";
+
+import { useNavigate } from "react-router-dom";
+// ======================================================
+
 const CustomerHome = () => {
   const { user } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <main className="min-h-[calc(100vh-4rem)] bg-gray-50 px-6 py-10">
@@ -20,11 +27,31 @@ const CustomerHome = () => {
 
         <div className="mt-6 grid gap-5 md:grid-cols-3">
           {[
-            ["Equipment", "Browse and rent camera equipment."],
+            ["Equipment", "Browse and rent camera equipment.", "/customer/equipment"],
             ["Photographers", "Discover professionals for your next shoot."],
             ["Bookings", "Track your upcoming rental and photography bookings."],
           ].map(([title, description]) => (
-            <div key={title} className="rounded-2xl border border-gray-200 bg-white p-6 shadow-sm">
+              // ==================================================================================
+              // make card clickable by thasindu ===========
+              <div
+                  key={title}
+                  onClick={() => {
+                    if (title === "Equipment") {
+                      navigate("/customer/equipment");
+                    }
+
+                    if (title === "Bookings") {
+                      navigate("/customer/rentals");
+                    }
+                  }}
+                  className={`rounded-2xl border border-gray-200 bg-white p-6 shadow-sm ${
+                    title === "Equipment"|| title === "Bookings"
+                    ? "cursor-pointer transition hover:-translate-y-1 hover:shadow-md"
+                    : ""
+                  }`}
+              >
+              
+
               <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-orange-50 font-bold text-orange-600">
                 {title.charAt(0)}
               </div>
